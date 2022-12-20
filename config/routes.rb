dac2de5ca55d58b3dec9ef4c20ecc114f79585f0
lou-GH <117365073+lou-GH@users.noreply.github.com>
2022-12-20 14:19:05 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,72 +1,73 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-    get 'order_details/update'
+    get 'order_details/:id', to: 'order_details#update'
   end
   namespace :admin do
-    get 'orders/show'
-    get 'orders/update'
+    get 'orders/:id', to: 'orders#show'
+    get 'orders/:id', to: 'orders#update'
   end
   namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
+    get 'customers', to: 'customers#index'
+    get 'customers/:id', to: 'customers#show'
+    get 'customers/:id/edit', to: 'customers#edit'
+    get 'customers/:id', to: 'customers#update'
   end
   namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
+    get 'genres', to: 'genres#index'
+    get 'genres', to: 'genres#create'
+    get 'genres/:id/edit', to: 'genres#edit'
+    get 'genres/:id', to: 'genres#update'
   end
   namespace :admin do
-    get 'items/index'
+    get 'items', to: 'items#index'
     get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
+    get 'items', to: 'items#create'
+    get 'items/:id', to: 'items#show'
+    get 'items/:id/edit', to: 'items#edit'
+    get 'items/:id', to: 'items#update'
   end
   namespace :admin do
-    get 'homes/top'
+    root to: 'homes#top'
   end
-  namespace :public do
-    get 'address/index'
-    get 'address/edit'
-    get 'address/create'
-    get 'address/update'
-    get 'address/destroy'
+  scope module: :public do
+    get 'addresses', to: 'address#index'
+    get 'addresses/:id/edit', to: 'address#edit'
+    get 'addresses', to: 'address#create'
+    get 'addresses/:id', to: 'address#update'
+    get 'addresses/:id', to: 'address#destroy'
   end
-  namespace :public do
+  scope module: :public do
     get 'orders/new'
     get 'orders/confirm'
     get 'orders/complete'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
+    get 'orders', to: 'cart_items#create'
+    get 'orders', to: 'cart_items#index'
+    get 'orders/:id', to: 'cart_items#show'
   end
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
+  scope module: :public do
+    get 'cart_items', to: 'cart_items#index'
+    get 'cart_items/:id', to: 'cart_items#update'
+    get 'cart_items/:id', to: 'cart_items#destroy'
     get 'cart_items/destroy_all'
-    get 'cart_items/create'
+    get 'cart_items', to: 'cart_items#create'
   end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/unsubscribe'
-    get 'customers/withdraw'
+  scope module: :public do
+    get 'customers/my_page', to: 'customers#show'
+    get 'customers/information/edit', to: 'customers#edit'
+    get 'customers/information', to: 'customers#update'
+    get 'customers/unsubscribe', to: 'customers#unsubscribe'
+    get 'customers/withdraw', to: 'customers#withdraw'
   end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
+  scope module: :public do
+    get 'items', to: 'items#index'
+    get 'items/:id', to: 'items#show'
   end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about', to: 'homes#about'
   end
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
