75b8b4161c173efab77576b73145842eeb6e57b0
lou-GH <117365073+lou-GH@users.noreply.github.com>
2022-12-20 15:54:47 +0000

    add genres

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,8 +1,19 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def create
+    @genre= Genre.new(genre_params)
+
+    if @genre.save
+      flash[:notice] = "genre was successfully created."
+      redirect_to admin_genres_path
+    else
+      @genres = Genre.all
+      render :admin_genres_path
+    end
   end
 
   def edit
@@ -10,4 +21,11 @@ class Admin::GenresController < ApplicationController
 
   def update
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
