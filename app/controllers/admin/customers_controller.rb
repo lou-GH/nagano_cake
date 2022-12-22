277806248825a714ed6c7fbf89485fa843df467c
lou-GH <117365073+lou-GH@users.noreply.github.com>
2022-12-22 15:25:43 +0000

    add custmer

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,8 +1,10 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
@@ -10,4 +12,11 @@ class Admin::CustomersController < ApplicationController
 
   def update
   end
+  
+  private
+
+  def customer_params
+    params.require(:customers).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,31 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-    get 'order_details/:id', to: 'order_details#update'
+    patch 'order_details/:id', to: 'order_details#update'
   end
   namespace :admin do
     get 'orders/:id', to: 'orders#show'
-    get 'orders/:id', to: 'orders#update'
+    patch 'orders/:id', to: 'orders#update'
   end
   namespace :admin do
     get 'customers', to: 'customers#index'
     get 'customers/:id', to: 'customers#show'
     get 'customers/:id/edit', to: 'customers#edit'
-    get 'customers/:id', to: 'customers#update'
+    patch 'customers/:id', to: 'customers#update'
   end
   namespace :admin do
-    post 'genres', to: 'genres#index'
-    get 'genres', to: 'genres#create'
+    get 'genres', to: 'genres#index'
+    post 'genres', to: 'genres#create'
     get 'genres/:id/edit', to: 'genres#edit'
-    get 'genres/:id', to: 'genres#update'
+    patch 'genres/:id', to: 'genres#update'
   end
   namespace :admin do
     get 'items', to: 'items#index'
     get 'items/new'
-    get 'items', to: 'items#create'
-    post 'items/:id', to: 'items#show'
+    post 'items', to: 'items#create'
+    get 'items/:id', to: 'items#show'
     get 'items/:id/edit', to: 'items#edit'
-    get 'items/:id', to: 'items#update'
+    patch 'items/:id', to: 'items#update'
   end
   namespace :admin do
     root to: 'homes#top'
@@ -33,31 +33,31 @@ Rails.application.routes.draw do
   scope module: :public do
     get 'addresses', to: 'address#index'
     get 'addresses/:id/edit', to: 'address#edit'
-    get 'addresses', to: 'address#create'
+    post 'addresses', to: 'address#create'
     get 'addresses/:id', to: 'address#update'
     get 'addresses/:id', to: 'address#destroy'
   end
   scope module: :public do
     get 'orders/new'
-    get 'orders/confirm'
+    post 'orders/confirm'
     get 'orders/complete'
-    get 'orders', to: 'cart_items#create'
+    post 'orders', to: 'cart_items#create'
     get 'orders', to: 'cart_items#index'
     get 'orders/:id', to: 'cart_items#show'
   end
   scope module: :public do
     get 'cart_items', to: 'cart_items#index'
-    get 'cart_items/:id', to: 'cart_items#update'
-    get 'cart_items/:id', to: 'cart_items#destroy'
-    get 'cart_items/destroy_all'
-    get 'cart_items', to: 'cart_items#create'
+    patch 'cart_items/:id', to: 'cart_items#update'
+    delete 'cart_items/:id', to: 'cart_items#destroy'
+    delete 'cart_items/destroy_all'
+    post 'cart_items', to: 'cart_items#create'
   end
   scope module: :public do
     get 'customers/my_page', to: 'customers#show'
     get 'customers/information/edit', to: 'customers#edit'
-    get 'customers/information', to: 'customers#update'
+    patch 'customers/information', to: 'customers#update'
     get 'customers/unsubscribe', to: 'customers#unsubscribe'
-    get 'customers/withdraw', to: 'customers#withdraw'
+    patch 'customers/withdraw', to: 'customers#withdraw'
   end
   scope module: :public do
     get 'items', to: 'items#index'
